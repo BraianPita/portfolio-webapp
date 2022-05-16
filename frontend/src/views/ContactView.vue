be6db49b9cadb8910bb1bcf31476fe07073f64ea
BraianPita <braianx100@gmail.com>
2022-05-16 18:45:45 -0500

    Added initital form for contact page

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -1,15 +1,47 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="contact p-3">
+    <form class="container-lg h-100" action="POST">
+      <h1 class="text-center">Get in Contact with Me</h1>
+
+      <hr>
+      
+      <div class="row">
+        <div class="col-md-6">
+          <label for="contact-email">Email</label>
+          <input class="form-control bg-dark" type="email" name="contact-email" placeholder="example@domain.com">
+        </div>
+        <div class="col-md-6">
+          <label for="contact-name">Name</label>
+          <input class="form-control bg-dark" type="text" name="contact-name" placeholder="Firstname Lastname">
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12">
+          <label for="contact-name">Topic</label>
+          <input class="form-control bg-dark" type="text" name="contact-topic" placeholder="Topic of the message">
+        </div>
+      </div>
+
+      <hr>
+
+      <textarea class="form-control bg-dark" name="contact=content" id="contact-content"></textarea>
+
+      <div class="d-flex"><button class="btn btn-primary my-3 mx-auto">Submit</button></div>
+    </form>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+
+  .contact {
+    min-height: 100%;
   }
-}
+
+  #contact-content {
+    resize: none;
+    width: 100%;
+    height: 30vh;
+  }
+
 </style>
