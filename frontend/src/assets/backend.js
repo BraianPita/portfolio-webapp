5daf3d49c4b274d6ce599677cd1dc590e609a9e3
BraianPita <braianx100@gmail.com>
2022-10-09 21:25:27 -0500

    added dynamic port to frontend axios

--- frontend/src/assets/backend.js
+++ frontend/src/assets/backend.js
@@ -1,9 +1,9 @@
 // axios import code
 import axios from 'axios';
 
-const port = process.env.PORT || 3000;
+const domain = process.env.DOMAIN || 'http://localhost:3000';
 
 // export axios object with baseURL 
 export const backend = axios.create({
-    baseURL: 'http://localhost:' + port + '/api/'
+    baseURL:  domain + '/api/'
   });
